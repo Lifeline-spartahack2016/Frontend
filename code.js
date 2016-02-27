46fd5b5bcda63d05edc169bea8cc1424edddcc26
Vivek Calambur <Vivek Calambur>
2016-02-27 14:12:07 -0600

    edited js

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
 $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
+
+var counter = 1;
+var limit = 3;
+function addInput(divName){
+     if (counter == limit)  {
+          alert("You have reached the limit of adding " + counter + " Emergency Contacts");
+     }
+     else {
+          var newdiv = document.createElement('contact');
+          var newdiv = document.createElement('phone');
+          newdiv.innerHTML = "Entry " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+          document.getElementById(divName).appendChild(newdiv);
+          counter++;
+     }
+}
