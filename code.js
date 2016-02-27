6673f25ac121d95a798ab6dc425ec2fb9c570bb2
Siddhant-Jain <Siddhant-Jain@users.noreply.github.com>
2016-02-27 16:57:53 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,17 +1,24 @@
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+$( document ).ready(function() {
+// console.log("ready");
+  $('.message a').click(function(){
+    // alert("kjhgjfjkl;jhgk");
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+ });
+ // alert("sdf");
 });
+
 
 var counter = 1;
 var limit = 3;
 function addInput(divName){
-     if (counter == limit)  {
-          alert("You have reached the limit of adding " + counter + " emergency contacts");
-     }
-     else {
+
           var newdiv = document.createElement('div');
-          newdiv.innerHTML = "Entry " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+          newdiv.innerHTML = " <br><input type='text' name='contact[]' placeholder = 'emergency contact name' id = 'name-" + (counter+1) + "' />" +
+                                  "<input type='text' name='phone[]' placeholder = 'emergency contact phone number' id = 'number-" + (counter+1) + "' />";
           document.getElementById(divName).appendChild(newdiv);
           counter++;
+          if (counter == (limit))  {
+               $("#addAnother").css("display", "none");
+
      }
 }
